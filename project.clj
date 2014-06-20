3d83c8b5c93195c38e3507cd94cc67f9e80b685d
Daniel E. Renfer <duck@kronkltd.net>
2014-06-20 17:45:38 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ciste/ciste-service-aleph "0.4.0-SNAPSHOT"
+(defproject ciste/ciste-service-aleph "0.4.0"
   :description "Aleph Connector Service for Ciste Applications"
-  :url "http://github.com/duck1123/ciste"
+  :url "http://github.com/duck1123/ciste-service-aleph"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
   :min-lein-version "2.0.0"
-  :dependencies [[aleph "0.3.0-alpha2"]
-                 [ciste/ciste-core "0.4.0-SNAPSHOT"]]
-  :plugins [[codox "0.6.1"]
-            [lein-midje "2.0.0-SNAPSHOT"]])
+  :dependencies [[aleph "0.3.0-beta8"]
+                 [ciste "0.4.0-SNAPSHOT"]]
+  :plugins [[codox "0.6.4"]
+            [lein-midje "2.0.3"]])
